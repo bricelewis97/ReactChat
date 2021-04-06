17531a64748b5ffcfc44be3fd0fadf4d53e5e984
bricelewis97 <79154799+bricelewis97@users.noreply.github.com>
2021-04-06 16:59:58 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBuT0ESJL-ngm2jjHi4R-WBXnFxY9ndDW0",
+    authDomain: "chatty-app-lab.firebaseapp.com",
+    projectId: "chatty-app-lab",
+    storageBucket: "chatty-app-lab.appspot.com",
+    messagingSenderId: "810622819386",
+    appId: "1:810622819386:web:9e47701d0bda0e8ecba903"
+};
